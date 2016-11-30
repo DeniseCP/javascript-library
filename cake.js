292e39d60351b99d240b6a73561bb09aac3d3e9f
DeniseCP <decpereira@hotmail.com>
2016-11-29 23:04:51 -0800

    commit
    commit changes

--- cake.js
+++ cake.js
@@ -1,31 +1,19 @@
-var data = [];
+var userlist = [];
 
 var btn = document.getElementById("find");
 var div = document.getElementById("option");
 var render = document.getElementById('renderBookList');
 
-window.onload = 	init();
-
-function loadJSON(callback){
-	var xobj = new XMLHttpRequest();
-	xobj.overrideMimeType("javascript-library");
-	xobj.open('GET',"books.json", true );
-
-	xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);   
-}
-
-function init(){
-	loadJSON(function(response){
-		data = JSON.parse(response);
-	});
-}
-
+var data = [
+	 {title: "The catcher in the rye", author: "JD Salinger", genre:"romance"},
+	 {title: "Lord of The Rings - The Fellowship of the Ring" , author: "JRR Tolkien", genre: "adventure"},
+	 {title: "How Grinch Stole Christmas", author: "Dr Seuss", genre:"children"},
+	 {title: "The Old Man and The Sea", author:"Ernest Hemingway", genre:"romance"},
+	 {title: "The Sun also Rises", author:"Ernest Hemingway", genre:"romance"},
+	 {title: "It", author:"Stephen King", genre:"terror"},
+	 {title: "I Robot", author:"Isaac Asimov", genre: "sci-fi"},
+	 {title: "Mind Transfer", author: "Janet Asimov", genre:"sci-fi"}
+	];
 
 btn.addEventListener("click", function(){
 	document.getElementById('renderBookList').innerHTML="";
@@ -107,5 +95,89 @@ function find(data, opt){
         
         li.innerHTML=li.innerHTML + "<strong>Author Name:</strong> "+ element.author +"; <strong>Book Name:</strong> "+ element.title;
 	}
+}
+
+function showAddForm(){
+    document.getElementById("addForm").classList.remove("hidden");
+    document.getElementById("addForm").style.display;
+    console.log("oi");
+}
+
+function generateLBCard() {
+	return Math.round(Math.random() * 50000);
+}
+
+function addNewUser() {
+    document.getElementById("message").innerHTML="";
+	let name = newUserForm.username.value;
+	let age = newUserForm.userage.value;
+	let card = generateLBCard();
+
+    if(name!==undefined && age!==undefined && name!=="" && age!==""){
+        var myUser = {
+		"name": name,
+		"age": age,
+		"librarycard": card
+	   };
+
+ 	  userlist.push(myUser);
+
+        messageSuccess(myUser);
+    }else{
+        messageError();
+    }
+	
+}
+
+function messageError(){
+    var p = document.createElement(p);
+    
+    p.setAttribute("id", "mgs_success");
+    
+    document.getElementById("message").appendChild(p);
+    p.style.color = "red";
+    p.style.fontWeight= "bold";
+    p.innerHTML+="Name and/or age cannot be null."
+}
+function messageSuccess(obj){
+    
+    var p = document.createElement(p);
+    
+    p.setAttribute("id", "mgs_success");
+    
+   document.getElementById("message").appendChild(p);
+    
+    p.innerHTML += "User: "+obj.name+ " added with success. Card Number: "+ obj.librarycard;  
+    
+    clearForm();
+}
+
+var clear = document.getElementById("clear");
+
+clear.addEventListener("click", function(){
+    document.getElementById("renderBookList").innerHTML=" ";
+});
+
+function clearForm(){
+    newUserForm.username.value="";
+    newUserForm.userage.value ="";
+}
+function listUsers(){
+    document.getElementById("addForm").classList.add("hidden");
+    document.getElementById("userlist").innerHTML="";
+    let t;
+    let ul = document.createElement("ul");
+    document.getElementById("userlist").style.display;
+    document.getElementById("userlist").appendChild(ul);
+    
+    let li = document.createElement("li");
+    
+    ul.appendChild(li); 
+   
+    
+    for(let i =0; i< userlist.length; i++){
+         t = document.createTextNode(userlist[i]);
+        li.innerHTML+="User name: "+userlist[i].name +". Library card: "+ userlist[i].librarycard;
+    }
 }
 
--- main.js
+++ main.js
@@ -1,53 +1,75 @@
-var data=[];
+var data = [];
 
-function openInaNewtab(url){
-	var win = window.open(url, '_blank');
+function openInaNewtab(url) {
+	var win = window.open(url);
 	win.focus();
-}	
-
-function loadJSON(callback){
-	var xobj = new XMLHttpRequest();
-	xobj.overrideMimeType("javascript-library");
-	xobj.open('GET',"users.json", true );
-
-	xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);   
-}
-
-function init(){
-	generateLBCard();
-	loadJSON(function(response){
-		data = JSON.parse(response);
-	});
 }
 
 
-function generateLBCard(){
-	var p = document.getElementById("library-card");
-	p.value = Math.round(Math.random()*50000);
+function generateLBCard() {
+	return Math.round(Math.random() * 50000);
 }
 
-window.onload = init();
+function addNewUser() {
+    document.getElementById("message").innerHTML="";
+	let name = newUserForm.username.value;
+	let age = newUserForm.userage.value;
+	let card = generateLBCard();
 
-function addNewUser(){
-	var name = newUserForm.username.value;
-	var age = newUserForm.userage.value;
-	var card = newUserForm.librarycard.value;
-
-	var myUser = {
+    if(name!==undefined && age!==undefined && name!=="" && age!==""){
+        var myUser = {
 		"name": name,
 		"age": age,
 		"librarycard": card
-	};
+	   };
 
- 	var d = JSON.stringify(myUser);
- 	data.push(d);
+ 	  var d = JSON.stringify(myUser);
+ 	  data.push(d);
 
+        messageSuccess(myUser);
+    }else{
+        messageError();
+    }
+	
+}
 
-	console.log("oi")
+function messageError(){
+    var p = document.createElement(p);
+    
+    p.setAttribute("id", "mgs_success");
+    
+   document.getElementById("message").appendChild(p);
+    p.style.color = "red";
+    p.style.fontWeight= "bold";
+    p.innerHTML+="Name and/or age cannot be null."
+}
+function messageSuccess(obj){
+    
+    var p = document.createElement(p);
+    
+    p.setAttribute("id", "mgs_success");
+    
+   document.getElementById("message").appendChild(p);
+    
+    p.innerHTML += "User: "+obj.name+ " added with success. Card Number: "+ obj.librarycard;  
+    
+    clearForm();
+}
+
+function clearForm(){
+    newUserForm.username.innerHTML="";
+    newUserForm.userage.innerHTML ="";
+}
+function listUsers(){
+    let ul = document.createElement("ul");
+    document.getElementById("userlist").style.display;
+    document.getElementById("userlist").appendChild(ul);
+    
+    let li = document.createElement("li");
+    
+    ul.appendChild(li);
+    
+    for(i =0; i< data; i++){
+        li.innerHTML+="User name: "+data[i].name +". Library card: "+ data[i].librarycard;
+    }
 }
